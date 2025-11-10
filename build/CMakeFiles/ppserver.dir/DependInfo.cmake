60f17816afce05da346be6f567593f0633fcafe9
ppy01 <3286412493@qq.com>
2025-11-10 22:13:29 +0800

    http_parser and handle mutex problme

---
+++ build/CMakeFiles/ppserver.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ppy001/project/ppserver/src/core/connection.cpp" "/home/ppy001/project/ppserver/build/CMakeFiles/ppserver.dir/src/core/connection.cpp.o"
+  "/home/ppy001/project/ppserver/src/core/connection_manager.cpp" "/home/ppy001/project/ppserver/build/CMakeFiles/ppserver.dir/src/core/connection_manager.cpp.o"
+  "/home/ppy001/project/ppserver/src/core/event_loop.cpp" "/home/ppy001/project/ppserver/build/CMakeFiles/ppserver.dir/src/core/event_loop.cpp.o"
+  "/home/ppy001/project/ppserver/src/core/main.cpp" "/home/ppy001/project/ppserver/build/CMakeFiles/ppserver.dir/src/core/main.cpp.o"
+  "/home/ppy001/project/ppserver/src/core/web_server.cpp" "/home/ppy001/project/ppserver/build/CMakeFiles/ppserver.dir/src/core/web_server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ppserver.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ppserver.dir/src/core/connection.cpp.o"
+  "CMakeFiles/ppserver.dir/src/core/connection_manager.cpp.o"
+  "CMakeFiles/ppserver.dir/src/core/event_loop.cpp.o"
+  "CMakeFiles/ppserver.dir/src/core/main.cpp.o"
+  "CMakeFiles/ppserver.dir/src/core/web_server.cpp.o"
+  "ppserver"
+  "ppserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ppserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
